3a20f7c3503994685f145fa9c3e43752f75e5575
Jivko03 <47494058+Jivko03@users.noreply.github.com>
2025-10-07 15:59:17 +0300

    Add files via upload

---
+++ Функционално порграмиране лаб/Ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex4
+{
+    public delegate double Calc(double x, double y);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calc add = (double x, double y) => { return x + y; };
+            Calc sub = (double x, double y) => { return x - y; };
+            Calc mult = (double x, double y) => { return x * y; };
+            Calc dev = (double x, double y) => { if (x != 0) { return x / y; } else { return 0; } };
+            Console.WriteLine(add(5,5));
+            Console.WriteLine(sub(5,5));
+            Console.WriteLine(mult(5,5));
+            Console.WriteLine(dev(5,5));
+        }
+    }
+}
---
+++ Функционално порграмиране лаб/Exer2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer2
+{
+    internal class Program
+    {
+        public delegate double FunctionX(double x);
+        static void Main(string[] args)
+        {
+            FunctionX fx1 = delegate (double x)
+            {
+                return Math.Pow(x, 2)+4*x - 5;
+            };
+            FunctionX fx2 = delegate (double x)
+            {
+                return Math.Pow(x, 3) - Math.Pow(x, 2) + 2 * x + 6;
+            };
+            FunctionX fx3 = delegate (double x)
+            {
+                if (x > 0) 
+                {
+                   return (3 * Math.Pow(x, 2) - 1) / Math.Sqrt(x);
+                }
+                else 
+                {
+                    return 0.0; 
+                }
+            };
+            FunctionX fx4 = delegate (double x)
+            {
+                if (x <= 0) { return 2; }
+                else return 0;
+            };
+            FunctionX fx5 = delegate (double x)
+            {
+                if (x>0 && x<1) { return x + 2; }
+                else if (x>=1 && x<=2) { return 3; }
+                else if (x>2 && x<3) { return 5 - x; }
+                else if (x >=3) { return 2; }
+                else { return 0; }
+            };
+            Console.WriteLine(fx1(2));
+            Console.WriteLine(fx2(2));
+            Console.WriteLine(fx3(1));
+            Console.WriteLine(fx4(0.5));
+            Console.WriteLine(fx4(1));
+            Console.WriteLine(fx4(2.5));
+            Console.WriteLine(fx4(3.5));
+
+        }
+    }
+}
